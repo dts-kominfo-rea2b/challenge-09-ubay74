5734673de629b89eebada4560bf9e4310cf078d5
ubay74 <105720990+ubay74@users.noreply.github.com>
2022-07-05 13:44:33 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,19 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
+    const {name, phone, email, photo} = data
     return (
-        <></>
+        <div className='card'>
+            <div className='image-contact'
+                <img src={photo} alt='broken' className='photo-contact' />
+            </div>
+            <div className='data-contact'>
+                <h1 className='name'>{name}</h1>
+                <h2 className='phone'>{phone}</h2>
+                <h2 className='email'>{email}</h2>
+            </div>
+        </div>
     )
 }
 
