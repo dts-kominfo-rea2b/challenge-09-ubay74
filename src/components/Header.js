1255e1c14f7e4add9684a57fa9f209958cbdf016
ubay74 <105720990+ubay74@users.noreply.github.com>
2022-07-05 13:54:07 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div>
             <h1 className='title'>Call A Friend</h1>
-            <h3 className='sub-title'>"your friendly contact app"</h3>
+            <h4 className='sub-title'>your friendly contact app</h4>
         </div>
     )
 }
