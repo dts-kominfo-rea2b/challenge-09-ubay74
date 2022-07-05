b70d62a6478e8f7d6aa04c91b0a5b1a28a2740eb
ubay74 <105720990+ubay74@users.noreply.github.com>
2022-07-05 13:50:20 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <div>
+            <h1 className='title'>Call A Friend</h1>
+            <h3 className='sub-title'>"your friendly contact app"</h3>
+        </div>
     )
 }
 
